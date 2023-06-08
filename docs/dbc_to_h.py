16a646269f0b225f8b0c4e067038b024bd8e60ee
Isabel Zhong <isabelzhong30@gmail.com>
2023-06-07 21:21:02 -0500

    bug fixes

--- docs/dbc_to_h.py
+++ docs/dbc_to_h.py
@@ -16,14 +16,14 @@ def dbc_to_h(dbc_file, h_file):
         for signal in message.signals:
             signalString = ""
             byteOrder = "Endian" if signal.byte_order == "big_endian" else ""
-            signalType = "Signed" if signal.is_signed == "Signed" else "Unsigned"
-            signalString = "Make" + byteOrder + signalType + "CANSignal(dataType," + str(signal.start) + "," + str(signal.length) + "," + str(signal.scale) + "," + str(signal.offset) + ") " + signal.name + "_Signal{};\n"
+            signalType = "Signed" if signal.is_signed else "Unsigned"
+            signalString = "Make" + byteOrder + signalType + "CANSignal(your_data_type," + str(signal.start) + "," + str(signal.length) + "," + str(signal.scale) + "," + str(signal.offset) + ") " + signal.name + "_Signal{};\n"
             with open(h_file, 'a') as file:
                 file.write(signalString)
             signals.append(signal.name + "_Signal")
         rx_message = "CANRXMessage<" + str(len(signals)) + "> " + message.name + "_RX_Message{can_bus, 0x" + format(message.frame_id, 'x') + ", " + ', '.join(signals) + "};\n"
         rx_messages += rx_message
-        tx_message = "CANTXMessage<" + str(len(signals)) + "> " + message.name + "_TX_Message{can_bus, 0x" + format(message.frame_id, 'x') + ", DLC, Freq, timer_group, " + ', '.join(signals) + "};\n"
+        tx_message = "CANTXMessage<" + str(len(signals)) + "> " + message.name + "_TX_Message{can_bus, 0x" + format(message.frame_id, 'x') + ", " + str(message.length) + ", your_freq, timer_group, " + ', '.join(signals) + "};\n"
         tx_messages += tx_message
     with open(h_file, 'a') as file:
         file.write("\n// RX Messages\n")
@@ -33,7 +33,7 @@ def dbc_to_h(dbc_file, h_file):
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
-        print("Usage: python dbc_to_csv.py <input_dbc_file> <output_h_file>")
+        print("Usage: python dbc_to_h.py <input_dbc_file> <output_h_file>")
         sys.exit(1)
 
     dbc_file = sys.argv[1]
