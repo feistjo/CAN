cc7c0ebb7cddef237a91bbbbe9f0360f3318e89b
feistjo <joshua.feist@gmail.com>
2023-07-11 15:27:49 -0700

    Updated dbc to h with multiplexed messages

--- docs/dbc_to_h.py
+++ docs/dbc_to_h.py
@@ -11,7 +11,9 @@ def dbc_to_h(dbc_file, h_file):
     with open(h_file, 'w') as file:
         file.write("// Signals\n")
     for message in db.messages:
+        multiplexor_signal = ""
         signals = []
+
         for signal in message.signals:
             signalString = ""
             if (signal.choices != None):
@@ -26,13 +28,39 @@ def dbc_to_h(dbc_file, h_file):
                 byteOrder = ""
                 endian = ""
             signalType = "Signed" if signal.is_signed else "Unsigned"
-            signalString += "Make" + byteOrder + signalType + "CANSignal(" + ("data_type_placeholder" if signal.choices == None else signal.name + "_Enum") + "," + str(signal.start) + "," + str(signal.length) + "," + str(signal.scale) + "," + str(signal.offset) + endian + ") " + signal.name + "_Signal{};\n"
+            signalString += "Make" + byteOrder + signalType + "CANSignal(" + ("data_type_placeholder" if signal.choices == None else signal.name + "_Enum") + "," + str(signal.start) + "," + str(signal.length) + "," + str(signal.scale) + ("" if isinstance(signal.scale, int) else "f") + "," + str(signal.offset) + endian + ") " + signal.name + "_Signal{};\n"
             with open(h_file, 'a') as file:
                 file.write(signalString)
-            signals.append(signal.name + "_Signal")
-        rx_message = "CANRXMessage<" + str(len(signals)) + "> " + message.name + "_RX_Message{can_bus, 0x" + format(message.frame_id, 'x') + ", " + ', '.join(signals) + "};\n"
+            if message.is_multiplexed():
+                if (signal.is_multiplexer):
+                    multiplexor_signal = signal.name + "_Signal_"
+                    continue
+                
+                found = False
+                for i in range(len(signals)):
+                    if signals[i][0] == signal.multiplexer_ids[0]:
+                        signals[i].append(signal.name + "_Signal_")
+                        found = True
+                        continue
+                if not found:
+                    signals.append([signal.multiplexer_ids[0], signal.name + "_Signal_"])
+            else:
+                signals.append(signal.name + "_Signal_")
+        print(signals)
+        if message.is_multiplexed():
+            signal_groups_str = ""
+            signal_groups = []
+            for i in range(len(signals)):
+                signal_groups_str += "MultiplexedSignalGroup<" + str(len(signals[i]) - 1) + "> " + message.name + "_SignalGroup_" + str(i) + "_{" + ', '.join(str(s) for s in signals[i]) + "};\n"
+                signal_groups.append(message.name + "_SignalGroup_" + str(i) + "_")
+            #MultiplexedCANTXMessage<2, uint8_t> tx_msg{can, 100, 8, 100, tx_multiplexor, tx_signals_0, tx_signals_1};
+            rx_message = signal_groups_str
+            rx_message += "MultiplexedCANRXMessage<" + str(len(signal_groups)) + ", " + "data_type_placeholder" + "> " + message.name + "_RX_Message_{can_bus, 0x" + format(message.frame_id, 'x') + ", " + multiplexor_signal + ", " + ', '.join(signal_groups) + "};\n"
+            tx_message = "MultiplexedCANTXMessage<" + str(len(signal_groups)) + ", " + "data_type_placeholder" + "> " + message.name + "_TX_Message_{can_bus, 0x" + format(message.frame_id, 'x') + ", " + ("true, " if message.is_extended_frame else "")  + str(message.length) + ", freq_placeholder, timer_group, " + multiplexor_signal + ", " + ', '.join(signal_groups) +  "};\n"
+        else:
+            rx_message = "CANRXMessage<" + str(len(signals)) + "> " + message.name + "_RX_Message_{can_bus, 0x" + format(message.frame_id, 'x') + ", " + ', '.join(signals) + "};\n"
+            tx_message = "CANTXMessage<" + str(len(signals)) + "> " + message.name + "_TX_Message_{can_bus, 0x" + format(message.frame_id, 'x') + ", " + ("true, " if message.is_extended_frame else "") + str(message.length) + ", freq_placeholder, timer_group, " + ', '.join(signals) + "};\n"
         rx_messages += rx_message
-        tx_message = "CANTXMessage<" + str(len(signals)) + "> " + message.name + "_TX_Message{can_bus, 0x" + format(message.frame_id, 'x') + ", " + str(message.length) + ", freq_placeholder, timer_group, " + ', '.join(signals) + "};\n"
         tx_messages += tx_message
     with open(h_file, 'a') as file:
         file.write("\n// RX Messages\n")
